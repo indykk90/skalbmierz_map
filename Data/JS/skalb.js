7e26d11cd3ed8f72a81ec20dea2593f59060e538
indykk90 <k.indelak@gmail.com>
2020-03-27 21:04:55 +0100

    pierwsza wersja

---
+++ Data/JS/skalb.js
@@ -0,0 +1,14 @@
+var poisSkalb = [ {
+"type": "FeatureCollection",
+"name": "skalb",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "location_4": null, "location_5": "Rosiejow, Woj. Swietokrzyskie, Poland", "Zabytek": "Kurhany w Rosiejowie" }, "geometry": { "type": "Point", "coordinates": [ 20.3195, 50.3333 ] } },
+{ "type": "Feature", "properties": { "location_4": "plac Marii Skłodowskiej-Curie", "location_5": "plac Marii Sklodowskiej-Curie, 28-530 Skalbmierz, Poland", "Zabytek": "Zabytkowy układ urbanistyczny w Skalbmierzu (rynek)" }, "geometry": { "type": "Point", "coordinates": [ 20.39966, 50.3201 ] } },
+{ "type": "Feature", "properties": { "location_4": null, "location_5": "Skalbmierz, Woj. Swietokrzyskie, Poland", "Zabytek": "Zbiornik retencyjno-rekreacyjny w Skalbmierzu" }, "geometry": { "type": "Point", "coordinates": [ 20.39528, 50.32673 ] } },
+{ "type": "Feature", "properties": { "location_4": null, "location_5": "Skalbmierz, Woj. Swietokrzyskie, Poland", "Zabytek": "Zabytkowy cmentarz zydowski w Skalbmierzu" }, "geometry": { "type": "Point", "coordinates": [ 20.3838, 50.31781 ] } },
+{ "type": "Feature", "properties": { "location_4": "ulica św. Jana Pawła II", "location_5": "ulica sw. Jana Pawła II Skalbmierz, Poland", "Zabytek": "Kolegiata w Skalbmierzu" }, "geometry": { "type": "Point", "coordinates": [ 20.39866, 50.31694 ] } },
+{ "type": "Feature", "properties": { "location_4": "ulica ppor. Sokoła", "location_5": "ulica ppor. Sokola 28-530 Skalbmierz, Poland", "Zabytek": "Stadion LKS Victoria Skalbmierz" }, "geometry": { "type": "Point", "coordinates": [ 20.39415, 50.32036 ] } }
+]
+}
+];
